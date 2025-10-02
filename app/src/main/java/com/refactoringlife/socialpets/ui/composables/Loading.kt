db5a3f64b5619659aef574e614dbb9cc22bbf287
Kevingg06 <kevingonzalezr433@gmail.com>
2025-10-02 19:52:33 -0300

    Creacion de la pantalla de carga generica

---
+++ app/src/main/java/com/refactoringlife/socialpets/ui/composables/Loading.kt
@@ -0,0 +1,43 @@
+package com.refactoringlife.socialpets.ui.composables
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.refactoringlife.socialpets.R
+import com.refactoringlife.socialpets.ui.theme.Gray80
+
+@Composable
+fun Loading(action: () -> Unit){
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Gray80)
+        ,
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clickable(onClick = action),
+                painter = painterResource(id = R.drawable.loading_icon),
+                contentDescription = "Icono de carga",
+                contentScale = ContentScale.Fit
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+    }
+}
