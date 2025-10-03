5413989dcdfe91ec949be02662f223c7d0e41c7d
Kevingg06 <kevingonzalezr433@gmail.com>
2025-10-03 16:56:07 -0300

    agregamos saltos de linea pertinentes y eliminamos imports innecesarios

--- app/src/main/java/com/refactoringlife/socialpets/ui/composables/Loading.kt
+++ app/src/main/java/com/refactoringlife/socialpets/ui/composables/Loading.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.refactoringlife.core.common.utils.Constants
 import com.refactoringlife.socialpets.R
