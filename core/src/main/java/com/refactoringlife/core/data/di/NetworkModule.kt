03af2b669de4206c3421bfc8df30d7ad6b61a74f
MavRial <maverickgshepherd@gmail.com>
2025-10-04 11:08:19 -0300

    [CHANGE] Refactor retrofit instance creation configuration to providers

--- core/src/main/java/com/refactoringlife/core/data/di/NetworkModule.kt
+++ core/src/main/java/com/refactoringlife/core/data/di/NetworkModule.kt
@@ -1,42 +1,28 @@
 package com.refactoringlife.core.data.di
 
+import com.refactoringlife.core.data.network.OKHttpProvider
+import com.refactoringlife.core.data.network.RetrofitProvider
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    private const val BASE_URL = "https://api.example.com/"
-
     @Provides
     @Singleton
     fun provideOKHttpClient(): OkHttpClient {
-        val logging = HttpLoggingInterceptor()
-        logging.level = HttpLoggingInterceptor.Level.BODY
-
-        return OkHttpClient.Builder()
-            .addInterceptor(logging)
-            .connectTimeout(30, TimeUnit.SECONDS)
-            .readTimeout(30, TimeUnit.SECONDS)
-            .build()
+        return OKHttpProvider.create()
     }
 
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .client(okHttpClient)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        return RetrofitProvider.create(okHttpClient)
     }
 }
