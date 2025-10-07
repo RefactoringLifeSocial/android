6ac948890463da740beb18c2f1e7dc19652fc47f
Kevingg06 <kevingonzalezr433@gmail.com>
2025-10-06 23:15:31 -0300

    Agregamos animación con Lottie

--- core/src/main/java/com/refactoringlife/core/common/ui/composables/Loading.kt
+++ core/src/main/java/com/refactoringlife/core/common/ui/composables/Loading.kt
@@ -1,25 +1,35 @@
 package com.refactoringlife.core.common.ui.composables
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.refactoringlife.core.R
 import com.refactoringlife.core.common.ui.theme.Gray80
 import com.refactoringlife.core.common.utils.Constants
 
 @Composable
-fun Loading(action: () -> Unit){
+fun Loading(){
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.loading_trail)
+    )
+
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -28,15 +38,12 @@ fun Loading(action: () -> Unit){
         contentAlignment = Alignment.Center
     ) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Image(
+            LottieAnimation(
+                composition = composition,
+                progress = { progress },
                 modifier = Modifier
-                    .size(60.dp)
-                    .clickable(onClick = action)
-                    .testTag(Constants.LOADING_ICON_TAG)
-                ,
-                painter = painterResource(id = R.drawable.loading_icon),
-                contentDescription = null,
-                contentScale = ContentScale.Fit
+                    .size(150.dp)
+                    .testTag(Constants.LOADING_TAG)
             )
         }
     }
--- core/src/main/java/com/refactoringlife/core/common/utils/Constants.kt
+++ core/src/main/java/com/refactoringlife/core/common/utils/Constants.kt
@@ -1,5 +1,5 @@
 package com.refactoringlife.core.common.utils
 
 object Constants {
-    const val LOADING_ICON_TAG = "LOADING_ICON_TAG"
+    const val LOADING_TAG = "LOADING_TAG"
 }
