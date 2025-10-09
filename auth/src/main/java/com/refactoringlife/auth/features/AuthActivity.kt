b1ef0211f7ab9aa056aa94e68b7e183ae0df76df
ricar <ricardosr4@gmail.com>
2025-10-09 12:12:26 -0300

    SCRUM-23 feat: add deep link handling custom feature, part two, config register fragment

--- auth/src/main/java/com/refactoringlife/auth/features/AuthActivity.kt
+++ auth/src/main/java/com/refactoringlife/auth/features/AuthActivity.kt
@@ -2,22 +2,23 @@ package com.refactoringlife.auth.features
 
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import com.refactoringlife.auth.R
-import com.refactoringlife.auth.features.login.presentation.fragment.LoginFragment
 
 class AuthActivity : FragmentActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
-        goToFragment()
+        setupNavigation()
     }
 
-    private fun goToFragment() {
-        val fragment = LoginFragment.createInstance()
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.view, fragment)
-            .commit()
+    private fun setupNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
