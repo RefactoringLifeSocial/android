bca1238f378140b0a8439804384695cb8bd41f91
ricar <ricardosr4@gmail.com>
2025-10-10 13:04:16 -0300

    SCRUM-23 feat: add deep link handling connect navigation, part 2

--- auth/src/main/java/com/refactoringlife/auth/features/AuthActivity.kt
+++ auth/src/main/java/com/refactoringlife/auth/features/AuthActivity.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.refactoringlife.auth.R
+import com.refactoringlife.core.common.utils.DeepLinks
 
 class AuthActivity : FragmentActivity() {
 
@@ -29,17 +30,22 @@ class AuthActivity : FragmentActivity() {
         val defaultFragment = intent.getStringExtra("default_fragment")
 
         when {
-            data?.path?.startsWith("/auth/login") == true -> {
+            data?.path?.startsWith("/${DeepLinks.Host.AUTH}${DeepLinks.Path.LOGIN}") == true -> {
                 navController.navigate(R.id.loginFragment)
             }
-            data?.path?.startsWith("/auth/register") == true -> {
+            data?.path?.startsWith("/${DeepLinks.Host.AUTH}${DeepLinks.Path.REGISTER}") == true -> {
                 navController.navigate(R.id.registerFragment)
             }
+//            data?.path?.startsWith("/auth/login") == true -> {
+//                navController.navigate(R.id.loginFragment)
+//            }
+//            data?.path?.startsWith("/auth/register") == true -> {
+//                navController.navigate(R.id.registerFragment)
+//            }
             // Pantalla por defecto
             defaultFragment == "login" -> {
                 navController.navigate(R.id.registerFragment)
             }
-
         }
     }
 }
