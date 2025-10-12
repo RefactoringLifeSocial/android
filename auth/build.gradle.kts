4db9643e1841c58d3b1972d307f3dce9b37a6204
MavRial <maverickgshepherd@gmail.com>
2025-10-12 14:11:44 -0300

    Refactor: remove unused auth dependencies, update TextFields with isError, add warning icon and error message, adjust colors to hexadecimal, remove previews, and pass tests

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -40,22 +40,9 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.material3.android)
-    implementation(libs.androidx.ui.tooling.preview.android)
-    implementation(libs.androidx.ui.test.junit4.android)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.kotlinx.coroutines.android)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(project(":core"))
+
+    implementation(libs.androidx.material.icons.extended)
     implementation(libs.hilt.android)
     kapt(libs.hilt.compiler)
-    implementation(libs.androidx.material.icons.extended)
-
-    // Module
-    implementation(project(":core"))
 }
