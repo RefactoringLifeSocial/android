fb8ee45a64a4ab4c669b326e41be2ca4967de595
kevinmaggio <kevinmaggio57@gmail.com>
2025-10-15 14:45:35 -0300

    Add fun deeplink

--- app/src/main/java/com/refactoringlife/socialpets/MainActivity.kt
+++ app/src/main/java/com/refactoringlife/socialpets/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.refactoringlife.socialpets
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import com.refactoringlife.auth.features.AuthActivity
+import com.refactoringlife.core.common.utils.AUTH_DEEPLINK
+import com.refactoringlife.core.common.utils.navigateToDeeplink
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        startActivity( Intent(this, AuthActivity::class.java))
+        navigateToDeeplink(AUTH_DEEPLINK)
         finish()
     }
 }
