076e2e1e785f113f8d7829b2c761662cbf46c15f
Santiago Gimenez <110056571+santixor2@users.noreply.github.com>
2025-10-15 20:56:39 -0300

    SCRUM-34 FIX

--- auth/src/main/java/com/refactoringlife/auth/features/login/domain/state/LoginState.kt
+++ auth/src/main/java/com/refactoringlife/auth/features/login/domain/state/LoginState.kt
@@ -3,9 +3,7 @@ package com.refactoringlife.auth.features.login.domain.state
 import com.refactoringlife.auth.features.login.domain.model.UserLoginModel
 
 data class LoginState(
-    val success: Boolean = false,
     val loading: Boolean = false,
-    val error: Boolean = false,
     val errorMessage : String? = "",
     val data : UserLoginModel? = null
 )
