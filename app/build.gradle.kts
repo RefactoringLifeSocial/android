8aa273ef0591a87152d6d6ac94109bd6cd71b2fe
Mariano Rial <maverickgshepherd@gmail.com>
2025-10-29 20:10:36 -0300

    - Added Theme.App.Starting for the native splash screen - Configured AndroidManifest so MainActivity displays the splash logo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,5 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.core.splashscreen)
 }
