6c9d9f4d6358c9136e6859dd4e21571a7320296f
ricar <ricardosr4@gmail.com>
2025-11-03 09:54:06 -0300

    SCRUM-47 feat: clean code

--- auth/src/main/java/com/refactoringlife/auth/features/forgotpassword/presentation/content/ResetPasswordView.kt
+++ auth/src/main/java/com/refactoringlife/auth/features/forgotpassword/presentation/content/ResetPasswordView.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.refactoringlife.auth.R
