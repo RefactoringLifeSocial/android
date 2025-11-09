fd8dc87e45fe17b43880bb3fc12141f9ab46cdf6
MavRial <108558141+MavRial@users.noreply.github.com>
2025-11-09 14:59:38 -0300

    [ADDED] SplashScreen setup using Android 12 API (#50)
    * [ADDED] SplashScreen setup using Android 12 API
    
    * [ADDED] App icon (logo) update
    
    * [ADDED] App icon (logo) update

--- app/src/main/java/com/refactoringlife/socialpets/MainActivity.kt
+++ app/src/main/java/com/refactoringlife/socialpets/MainActivity.kt
@@ -2,14 +2,19 @@ package com.refactoringlife.socialpets
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.refactoringlife.core.common.utils.AUTH_DEEPLINK
 import com.refactoringlife.core.common.utils.navigateToDeeplink
 import dagger.hilt.android.AndroidEntryPoint
 
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         navigateToDeeplink(AUTH_DEEPLINK)
         finish()
     }
