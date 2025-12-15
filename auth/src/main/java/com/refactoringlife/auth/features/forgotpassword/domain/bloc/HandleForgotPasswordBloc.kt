3b22a8b05d92d2ddccda8e957d878850c4abe967
Kevingg06 <kevingonzalezr433@gmail.com>
2025-12-14 21:17:35 -0300

    Navegacion a Login cuando se hace click al modal

--- auth/src/main/java/com/refactoringlife/auth/features/forgotpassword/domain/bloc/HandleForgotPasswordBloc.kt
+++ auth/src/main/java/com/refactoringlife/auth/features/forgotpassword/domain/bloc/HandleForgotPasswordBloc.kt
@@ -3,6 +3,7 @@ package com.refactoringlife.auth.features.forgotpassword.domain.bloc
 import com.refactoringlife.auth.features.forgotpassword.data.dto.request.UserSendEmailRequest
 import com.refactoringlife.auth.features.forgotpassword.domain.state.ForgotPasswordState
 import com.refactoringlife.auth.features.forgotpassword.domain.usecases.SendEmailResponseUseCase
+import com.refactoringlife.core.common.utils.isValidEmail
 
 class HandleForgotPasswordBloc(
     val sendEmailResponseUseCase: SendEmailResponseUseCase
@@ -18,7 +19,7 @@ class HandleForgotPasswordBloc(
             return
         }
 
-        if (event.email.isNotBlank()) {
+        if (event.email.isValidEmail()) {
             update {
                 it.copy(showModal = true)
             }
