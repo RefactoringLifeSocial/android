2e87ee93734df3800bdb5e7a483bfd7b40934134
ricardosr4 <ricardosr4@gmail.com>
2025-12-20 11:11:33 -0300

    clean code branch navigation 3 and update welcome register screen.

--- auth/src/main/java/com/refactoringlife/auth/features/register/presentation/screen/WelcomeRegisterScreen.kt
+++ auth/src/main/java/com/refactoringlife/auth/features/register/presentation/screen/WelcomeRegisterScreen.kt
@@ -1,9 +1,7 @@
 package com.refactoringlife.auth.features.register.presentation.screen
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import com.refactoringlife.auth.features.register.presentation.content.WelcomeRegisterView
-import com.refactoringlife.auth.features.register.presentation.viewmodel.RegisterViewModel
 
 @Composable
 fun WelcomeRegisterScreen(
